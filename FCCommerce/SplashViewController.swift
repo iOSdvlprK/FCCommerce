c01cbddf6e991e89eb51c08d00c3db43df581c87
iOSdvlprK <iOSdvlprK@gmail.com>
2026-09-01 23:12:09 +0900

    add splash screen and update launch screen with app icon
    - add app-icon image set to Assets.xcassets
    - update LaunchScreen.storyboard with centered app icon
    - add Splash.storyboard and SplashViewController
    - update Info.plist to route initial scene to Splash storyboard

---
+++ FCCommerce/SplashViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SplashViewController.swift
+//  FCCommerce
+//
+//  Created by joe on 9/1/26.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .purple
+    }
+}
