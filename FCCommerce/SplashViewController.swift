89865a10d77ae17f0c04920f8904b6f2956263b3
iOSdvlprK <iOSdvlprK@gmail.com>
2026-09-02 23:12:50 +0900

    replace static splash icon with Lottie animation
    - add lottie-ios SPM package dependency and Package.resolved
    - add SplashLottie animation JSON to Assets
    - replace UIImageView with LottieAnimationView in Splash.storyboard
    - play Lottie animation in SplashViewController viewDidAppear

--- FCCommerce/SplashViewController.swift
+++ FCCommerce/SplashViewController.swift
@@ -6,11 +6,10 @@
 //
 
 import UIKit
+import Lottie
 
 class SplashViewController: UIViewController {
-    @IBOutlet weak var appIconCenterYConstraint: NSLayoutConstraint!
-    @IBOutlet weak var appIconCenterXConstraint: NSLayoutConstraint!
-    @IBOutlet weak var appIcon: UIImageView!
+    @IBOutlet weak var lottieAnimationView: LottieAnimationView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,18 +17,7 @@ class SplashViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        /*
-        appIconCenterXConstraint.constant = -(view.frame.width / 2) - (appIcon.frame.width / 2)
-        appIconCenterYConstraint.constant = -(view.frame.height / 2) - (appIcon.frame.height / 2)
         
-        UIView.animate(withDuration: 1) { [weak self] in
-            self?.view.layoutIfNeeded()
-        }
-        */
-        
-        UIView.animate(withDuration: 3, animations: { [weak self] in
-            let rotationAngle = CGFloat.pi
-            self?.appIcon.transform = CGAffineTransform(rotationAngle: rotationAngle)
-        })
+        lottieAnimationView.play()
     }
 }
