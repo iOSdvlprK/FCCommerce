37f9700b302d20646af91aa303e2e8b73885779f
iOSdvlprK <iOSdvlprK@gmail.com>
2026-09-03 23:18:37 +0900

    add DesignSource assets and helper enums (CPColor, CPFont, CPImage)
    - add Colors.xcassets and Images.xcassets with design resources
    - add CPColor, CPFont, and CPImage helper enums for standardized styling

---
+++ FCCommerce/DesignSource/CPColor.swift
@@ -0,0 +1,19 @@
+//
+//  CPColor.swift
+//  FCCommerce
+//
+//  Created by joe on 9/3/26.
+//
+
+import UIKit
+
+enum CPColor {
+    static var bk: UIColor = UIColor(resource: .bk)
+    static var wh: UIColor = UIColor(resource: .wh)
+    static var coral: UIColor = UIColor(resource: .coral)
+    static var yellow: UIColor = UIColor(resource: .yellow)
+    static var keyColorRed: UIColor = UIColor(resource: .keyColorRed)
+    static var keyColorRed2: UIColor = UIColor(resource: .keyColorRed2)
+    static var keyColorBlue: UIColor = UIColor(resource: .keyColorBlue)
+    static var keyColorBlueTrans: UIColor = UIColor(resource: .keyColorBlueTrans)
+}
