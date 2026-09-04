aa14f211b3f3955c58071d0a33b984a91febbd51
iOSdvlprK <iOSdvlprK@gmail.com>
2026-09-04 23:30:57 +0900

    add HomeViewController and transition from splash screen
    - remove default Main.storyboard and ViewController
    - add Home.storyboard and HomeViewController
    - transition window rootViewController to HomeViewController after splash animation

---
+++ FCCommerce/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  FCCommerce
+//
+//  Created by joe on 9/4/26.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- FCCommerce/SplashViewController.swift
+++ FCCommerce/SplashViewController.swift
@@ -18,6 +18,14 @@ class SplashViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        lottieAnimationView.play()
+        lottieAnimationView.play { _ in
+            let storyboard = UIStoryboard(name: "Home", bundle: nil)
+            let viewController = storyboard.instantiateInitialViewController()
+            
+            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+               let window = windowScene.windows.first(where: { $0.isKeyWindow }) {
+                window.rootViewController = viewController
+            }
+        }
     }
 }
