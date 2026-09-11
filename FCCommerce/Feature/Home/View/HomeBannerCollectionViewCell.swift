3d12f50d5ededd14b2cbfc9b671e3547b92030ed
iOSdvlprK <iOSdvlprK@gmail.com>
2026-09-11 23:40:54 +0900

    fetch and display home data from network API
    - add NetworkService with async getHomeData()
    - add HomeResponse, Banner, and Product Decodable models
    - update HomeBannerCollectionViewCellViewModel to use image URL
    - replace mock data in HomeViewController with API response

--- FCCommerce/Feature/Home/View/HomeBannerCollectionViewCell.swift
+++ FCCommerce/Feature/Home/View/HomeBannerCollectionViewCell.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 struct HomeBannerCollectionViewCellViewModel: Hashable {
-    let bannerImage: UIImage
+    let bannerImageUrl: String
 }
 
 class HomeBannerCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
     
     func setViewModel(_ viewModel: HomeBannerCollectionViewCellViewModel) {
-        imageView.image = viewModel.bannerImage
+//        imageView.image = viewModel.bannerImage
     }
 }
 
